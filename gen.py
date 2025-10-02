b6f0991c89e9a28e4cf9a9101015abd7581e80a1
Leo6485 <lo570354@gmail.com>
2025-10-02 20:01:21 -0300

    Add initial implementation of genetic algorithm with input handling and data visualization

---
+++ gen.py
@@ -0,0 +1,135 @@
+from random import *
+from math import *
+from copy import deepcopy
+import numpy as np
+
+
+class Individuo:
+    def __init__(self, size, chromo=None):
+        if not chromo:
+            chromo = list(range(size))
+            shuffle(chromo)
+
+        self.chromo = chromo
+        self.size = size
+        self.y = None
+
+    def fit(self, funct):
+        self.y = funct(self.chromo)
+
+    def mut(self, prob):
+        if random() <= prob:
+            idx = [randint(0, self.size - 1) for i in range(2)]
+
+            bak = self.chromo[idx[0]]
+            self.chromo[idx[0]] = self.chromo[idx[1]]
+            self.chromo[idx[1]] = bak
+
+class Ag:
+    def __init__(self, size=100, elite_size=2, mut_prob=0.01, cross_prob=1, chromo_size=16, sel_mode=1, funct=lambda x:1):
+        self.size = size
+        self.population = [Individuo(size=chromo_size) for i in range(size)]
+        self.chromo_size = chromo_size
+
+        self.elite_size = elite_size
+        self.elite = []
+
+        self.funct = funct
+        self.sel_mode = sel_mode
+        self.mut_prob = mut_prob
+        self.cross_prob = cross_prob
+        
+        self.data = []
+
+    def update_elite(self):
+        self.population.sort(key=lambda x: x.y)
+        self.elite = deepcopy(self.population[:self.elite_size])
+
+    @staticmethod
+    def w_sample(l, w, k):
+        result = []
+        ids = range(len(l))
+        for _ in range(k):
+            x = choices(ids, weights=w, k=1)[0]
+            w[x] = 0
+            result.append(l[x])
+
+        return result
+    
+    def roleta(self):
+        return choices(self.population, weights=[x.y for x in self.population], k=2)
+  
+    def torneio(self, k=5):
+        pai1 = min(choices(self.population, k=k), key=lambda x: x.y)
+        pai2 = min(choices(self.population, k=k), key=lambda x: x.y)
+        
+        return pai1, pai2
+
+    def cross(self):
+        len_ = randint(1, self.chromo_size)
+
+        s_idx = randint(0, self.chromo_size - len_)
+        e_idx = s_idx + len_
+
+        if self.sel_mode:
+            p1, p2 = self.roleta()
+        else:
+            p1, p2 = self.torneio()
+        
+        c1, c2 = p1.chromo, p2.chromo
+
+        o1_m = c2[s_idx:e_idx+1]
+        o2_m = c1[s_idx:e_idx+1]
+
+        f = self.chromo_size - len_
+        o1_right = [i for i in c1[e_idx+1:] + c1[:e_idx+1] if i not in o1_m][:f]
+        o2_right = [i for i in c2[e_idx+1:] + c2[:e_idx+1] if i not in o2_m][:f]
+
+        o1_left = [i for i in c2[:e_idx+1] if i not in o1_m + o1_right]
+        o2_left = [i for i in c1[:e_idx+1] if i not in o2_m + o2_right]
+
+        o1 = o1_left + o1_m + o1_right
+        o2 = o2_left + o2_m + o2_right
+
+        if random() <= self.cross_prob:
+            return Individuo(size=self.chromo_size, chromo=o1), Individuo(size=self.chromo_size, chromo=o2)
+        else:
+            return p1, p2
+
+    def cross_all(self):
+        new_pop = []
+        while len(new_pop) < self.size:
+            o1, o2 = self.cross()
+            new_pop.append(o1)
+            new_pop.append(o2)
+
+        self.population = new_pop
+
+    def mut(self):
+        for x in self.population:
+            x.mut(self.mut_prob)
+
+    def show(self):
+        if len(self.elite):
+            print(f"Melhor fitness: {self.elite[0].y} {len(self.elite[0].chromo)}")
+    
+    def save_data(self):
+        y = [x.y for x in self.population]
+        mean = np.mean(y)
+        median = np.median(y)
+        std = np.std(y)
+
+        self.data.append((self.elite[0].y, max(y), min(y), mean, median, std))
+
+    def fit(self, generations):
+        for _ in range(generations):
+            for individuo in self.population:
+                individuo.fit(self.funct)
+
+            self.update_elite()
+            self.show()
+            self.save_data()
+            self.cross_all()
+            self.mut()
+            
+            self.population.extend(deepcopy(self.elite))
---
+++ main.py
@@ -0,0 +1,148 @@
+from gen import Ag
+import csv
+import matplotlib.pyplot as plt
+
+
+
+
+
+from copy import copy
+
+
+def read(filename):
+    global num_tasks, num_machines, times
+    with open(filename, "r") as f:
+        data = f.read().split("\n")
+        num_tasks, num_machines = map(int, data[0].split())
+        
+        times = [[0, set()] for i in range(num_tasks)]
+
+        for i, line in enumerate(data[1:]):
+            line_data = list(map(int, line.split()))
+            
+            times[i][0] = line_data[0]
+
+            for task in line_data[1:]:
+                times[task - 1][1].add(i)
+
+def calc(chromo):
+    global times, num_machines
+
+    machines = [[0, 0] for i in range(num_machines)]
+    ref_task = [-1] * num_machines
+    concluidas = set()
+    chromo = copy(chromo)
+
+    while chromo:
+        new_chromo = []
+
+        for task in chromo:
+            idx = machines.index(min(machines, key=lambda x: x[0]))
+            if times[task][1].issubset(concluidas):
+                ref_task[idx] = task
+                machines[idx][1] = times[task][0] # Tempo restante
+            else:
+                new_chromo.append(task)
+            
+            
+            mtl = [i[1] for i in machines]
+            temp = sorted(list(filter(lambda x: x, mtl)))
+            temp.append(0)
+            min_machine = temp[0]
+
+            for i in range(len(machines)):
+                if machines[i][1] == 0: continue
+
+                machines[i][1] -= min_machine
+                machines[i][0] += min_machine
+                if machines[i][1] == 0:
+                    concluidas.add(ref_task[i])
+
+        chromo = new_chromo
+    return max(machines)[0]
+
+
+num_tasks = None
+num_machines = None
+times = None
+read("in2.txt")
+
+
+population_size = [100]
+elite_size = [2]
+mut_prob = [0.05]
+cross_prob = [0.8]
+sel_mode = [1]
+geracoes = [100]
+
+all_data = []
+
+for g in geracoes:
+    for i in population_size:
+        for e in elite_size:
+            for m in mut_prob:
+                for c in cross_prob:
+                    for sel in sel_mode:
+                        ag = Ag(i, chromo_size=num_tasks, mut_prob=m, cross_prob=c, elite_size=e, funct=calc, sel_mode=sel)
+                        ag.fit(g)
+                        mode = "roleta" if sel else "torneio"
+                        all_data.append((ag.data, (m, e, i, c, mode, g)))
+
+
+best_data, best_params = min(all_data, key=lambda x: x[0][-1][2])
+print(f"Melhor execução: {best_params}\nDistância: {best_data[-1][2]}")
+gens = list(range(1, len(best_data) + 1))
+
+elite_vals = [t[0] for t in best_data]
+plt.figure(figsize=(8, 5), dpi=300)
+plt.plot(gens, elite_vals, linewidth=2, marker='o', markersize=4)
+plt.xlabel('Geração')
+plt.ylabel('Distância')
+plt.title('Melhor Individuo')
+plt.grid(True, linestyle='--', alpha=0.5)
+plt.tight_layout()
+plt.savefig('best.png')
+plt.close()
+
+max_vals    = [t[1] for t in best_data]
+min_vals    = [t[2] for t in best_data]
+mean_vals   = [t[3] for t in best_data]
+median_vals = [t[4] for t in best_data]
+
+plt.figure(figsize=(8, 5), dpi=300)
+plt.plot(gens, min_vals,    label='Melhor',  linewidth=2)
+plt.plot(gens, max_vals,    label='Pior',    linewidth=2)
+plt.plot(gens, mean_vals,   label='Média',   linewidth=2)
+plt.plot(gens, median_vals, label='Mediana', linewidth=2)
+plt.xlabel('Geração')
+plt.ylabel('Distância')
+plt.title('Dados da Melhor Execução')
+plt.legend()
+plt.grid(True, linestyle='--', alpha=0.5)
+plt.tight_layout()
+plt.savefig('best_2.png')
+plt.close()
+
+plt.figure(figsize=(10, 6), dpi=300)
+
+for idx, (run_data, params) in enumerate(all_data, start=1):
+    mean_run = [t[3] for t in run_data]
+    gens_run = list(range(1, len(run_data) + 1))
+    plt.plot(gens_run, mean_run, linewidth=1.5, alpha=0.7, label=f'Run {idx}')
+
+plt.xlabel('Geração')
+plt.ylabel('Distância Média')
+plt.title('Distância Média de Cada Execução')
+plt.legend(fontsize='small', ncol=2)
+plt.grid(True, linestyle='--', alpha=0.5)
+plt.tight_layout()
+plt.savefig('all.png')
+plt.close()
+
+with open('all_data.csv', 'w', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerow(['mutate_prob', 'elite_size', 'individuos', 'cross_prob', 'selecao', 'geracoes', 'geracao', 'elite', 'max', 'min', 'mean', 'median', 'std'])
+    for run_data, (m, e, i, c, sel, gen_total) in all_data:
+        for idx, values in enumerate(run_data, start=1):
+            elite, mx, mn, mean, median, std = values
+            writer.writerow([m, e, i, c, sel, gen_total, idx, elite, mx, mn, mean, median, std])
